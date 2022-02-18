bf64e37ee77fc2b3c279c6961ac265956f681240
LBechara <becharalucas@hotmail.com>
2022-02-18 15:14:42 -0300

    subindo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/model/todo.model';
 
 @Component({
@@ -6,12 +7,33 @@ import { Todo } from 'src/model/todo.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   todoList: Todo[] = []
+  form: FormGroup | any
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.required
+      ])]
+    })
     this.todoList.push(new Todo( 1, 'ir a feira', false))
     this.todoList.push(new Todo( 2, 'curar da covid', false))
     this.todoList.push(new Todo( 3, 'escovar os dentes', true))
+  }
+
+  ngOnInit(): void {
+  }
+
+  alteraStatusTarefa(tarefa: Todo, feito: boolean): void {
+    tarefa.finalizado = feito
+  }
+
+  excluiTarefa(tarefa: Todo) {
+    const index = this.todoList.indexOf(tarefa)
+    if(index != -1) {
+      this.todoList.splice(index, 1)
+    }
   }
 }
